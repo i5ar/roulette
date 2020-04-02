d9f9b51d8daa2ff2de40f991886e9523ae47ee99
unknown <pierpaolo.rasicci@iispascal.it>
2020-04-02 20:30:38 +0200

    Update script

--- __main__.py
+++ __main__.py
@@ -1,14 +1,39 @@
 from pathlib import Path
-from shutil import copy, copytree, rmtree, ignore_patterns
+import argparse
+from shutil import copy, copytree, ignore_patterns
 
-back_end = input('please, insert back-end directory root: ')
-back_end_path = Path(back_end)
 
-# Copy roulette-front/static/ to roulette-back/static/
-static_path = Path('static')
-rmtree(back_end_path / "static", ignore_errors=True)
-copytree(static_path, back_end_path / "static", ignore=ignore_patterns('*.pyc'))
+def main():
+    # https://docs.python.org/3/library/argparse.html
+    parser = argparse.ArgumentParser(description='Copy static folder and html')
+    parser.add_argument(
+        '-p',
+        '--path',
+        type=Path,
+        required=True,
+        help='back-end directory root')
+    args = parser.parse_args()
 
-# Copy roulette-front/index.html to roulette-back/templates/index.html
-index_path = Path('index.html')
-copy(index_path, back_end_path / "templates")
+    # back_end = input('please, insert back-end directory root: ')
+    # back_end_path = Path(back_end)
+    back_end_path = args.path
+
+    # NOTE: Copy roulette-front/static/ to roulette-back/static/
+    static_path = Path('static')
+    print('Copying "static/"')
+    copytree(
+        static_path,
+        back_end_path / "static",
+        ignore=ignore_patterns('*.pyc'),
+        copy_function=copy,
+        dirs_exist_ok=True)
+
+    # NOTE: Copy roulette-front/index.html to roulette-back/templates/index.html
+    index_path = Path('index.html')
+    print('Copying "index.html"')
+    copy(index_path, back_end_path / "templates")
+    print('Done')
+
+
+if __name__ == "__main__":
+    main()
